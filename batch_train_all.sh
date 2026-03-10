6e1820b2f2b71aa796d7e3e7e40eaa2afb2eb1b8
aaronlau_ouhpc <Shouyue.Liu-1@ou.edu>
2026-03-10 11:59:28 -0500

    final experiments

---
+++ batch_train_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+./batch_train_cifar10_gat.conda.sh
+./batch_train_cifar10_gcn.conda.sh
+./batch_train_cifar10_resnet.conda.sh
+./batch_train_imagewoof_resnet.conda.sh
+./batch_train_imagewoof_gat.conda.sh
+./batch_train_imagewoof_gcn.conda.sh
+./batch_train_imagenette2_gat.conda.sh
+./batch_train_imagenette2_gcn.conda.sh
+./batch_train_imagenette2_resnet.conda.sh
